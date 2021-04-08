0a2c94bdc6d7b0822597c78b05d62f655a88362d
Zedespook <vasszili@gmail.com>
2021-04-08 20:44:43 +0200

    Update 4/8/2021 20:44

--- godot/src/test/player/player.gd
+++ godot/src/test/player/player.gd
@@ -3,6 +3,9 @@ extends RigidBody
 # Global-local variable for the force direction.
 var _direction: Vector3 = Vector3.ZERO
 
+# Ground checking raycast.
+onready var ray = $RayCast
+
 
 func _process(_delta: float) -> void:
 	# Get the direction from input.
@@ -13,10 +16,6 @@ func _physics_process(_delta: float) -> void:
 	# Put the player back to the start if they fall.
 	if global_transform.origin.y < -5:
 		var _reload: int = get_tree().reload_current_scene()
-
-	# If nothing is pressed, why go further?
-	if _direction == Vector3.ZERO:
-		return
 
 	# Apply the force to the rigid-body.
 	apply_central_impulse(_direction)
@@ -31,4 +30,4 @@ func _get_direction() -> void:
 	_direction.x = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
 	# Normalise the vector
-	_direction = _direction.normalized()
+	_direction = _direction.normalized() / 5
